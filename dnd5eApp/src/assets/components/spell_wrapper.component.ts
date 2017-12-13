9c7ccf5ec9353bddc3314ac8303da7eba072a050
apwete <wil_driver@yahoo.com>
2017-12-13 07:43:41 -0500

    Angular skeliton
    Again this is just the frame to build in.

---
+++ dnd5eApp/src/assets/components/spell_wrapper.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'spell-wrapper',
+  templateUrl: '/assets/templates/spell_wrapper.html',
+})
+export class SpellWrapperComponent {
+  title = 'Spell Wrapper';
+}
